0250068d658504350d60cf1be7b92f543494415c
jshli <joshua.li@mu-aa.com>
2019-05-01 22:29:42 +1000

    create login

--- src/pages/confirm.js
+++ src/pages/confirm.js
@@ -1,19 +1,19 @@
 import React from "react"
 import GoTrue from "gotrue-js"
 
-// const auth = new GoTrue({
-//     APIUrl: "https://niche.netlify.com/.netlify/identity",
-//     audience: "",
-//     setCookie: false
-// })
+const auth = new GoTrue({
+    APIUrl: "https://niche.netlify.com/.netlify/identity",
+    audience: "",
+    setCookie: false
+})
 
-// auth
-// .confirm(token)
-// .then(response => console.log("confirmed", response))
-// .catch(error => console.log("error", error))
+auth
+.confirm(token)
+.then(response => console.log("confirmed", response))
+.catch(error => console.log("error", error))
 
-// export default function Confirm(props) {
-//     return (
-//         <div></div>
-//     )
-// }
+export default function Confirm(props) {
+    return (
+        <div>dashboard</div>
+    )
+}
